e4c93aed7f4121a76e0770d09e44cfc42735d911
Sergey <serg.mayorov316@gmail.com>
2024-04-16 17:59:48 +1000

    start commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace dIplom3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cursorButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.wallButton = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cursorButton);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.wallButton);
+            this.panel1.Location = new System.Drawing.Point(12, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(58, 435);
+            this.panel1.TabIndex = 0;
+            // 
+            // cursorButton
+            // 
+            this.cursorButton.Location = new System.Drawing.Point(0, 122);
+            this.cursorButton.Name = "cursorButton";
+            this.cursorButton.Size = new System.Drawing.Size(57, 55);
+            this.cursorButton.TabIndex = 2;
+            this.cursorButton.Text = "Выделение";
+            this.cursorButton.UseVisualStyleBackColor = true;
+            this.cursorButton.Click += new System.EventHandler(this.cursorButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Звук";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // wallButton
+            // 
+            this.wallButton.Location = new System.Drawing.Point(0, 0);
+            this.wallButton.Name = "wallButton";
+            this.wallButton.Size = new System.Drawing.Size(57, 53);
+            this.wallButton.TabIndex = 0;
+            this.wallButton.Text = "Стена\r\n";
+            this.wallButton.UseVisualStyleBackColor = true;
+            this.wallButton.Click += new System.EventHandler(this.wallButtonClick);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(354, 35);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(753, 435);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 504);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button wallButton;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button cursorButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dIplom3
+{
+    public partial class Form1 : Form
+    {
+        private Control activeTool;
+        private Point? previousPoint = null;
+        private List<Line> lines = new List<Line>();
+        private Line selectedLine = null;
+        public Form1()
+        {
+            InitializeComponent();
+            canvas.MouseClick += canvasClick;
+            canvas.Paint += canvasPaint;
+        }
+
+        private void wallButtonClick(object sender, EventArgs e)
+        {
+            activeTool = sender as Control;
+            (sender as Button).BackColor = Color.Red;
+        }
+
+        private void canvasPaint(object sender, PaintEventArgs e)
+        {
+            foreach (var line in lines)
+            {
+                line.Draw(e.Graphics);
+            }
+        }
+
+        private void canvasClick(object sender, MouseEventArgs e)
+        {
+            if (activeTool != null && activeTool.Equals(wallButton))  
+            {
+                if (previousPoint is null)
+                {
+                    previousPoint = e.Location;
+                }
+                else
+                {
+                    using (Graphics g = canvas.CreateGraphics())
+                    {
+                        g.DrawLine(Pens.Black, previousPoint.Value, e.Location);
+                        lines.Add(new Line(previousPoint, e.Location, LineType.Straight));
+                        previousPoint = null;
+                    }
+                }
+            }
+            else if (activeTool != null && activeTool.Equals(cursorButton))
+            {
+                foreach (var line in lines)
+                {
+                    if (line.hitTest(e.Location))
+                    {
+                        selectedLine = line;
+                        selectedLine.Selected = true;
+                        canvas.Invalidate();
+                        return;
+                    }
+                }
+                selectedLine = null;
+                foreach (var line in lines)
+                {
+                    line.Selected = false;
+                }
+                canvas.Invalidate();
+            }
+        }
+
+        private void cursorButton_Click(object sender, EventArgs e)
+        {
+            activeTool = sender as Control;
+            (sender as Button).BackColor = Color.Red;
+        }
+    }
+    public class Line
+    {
+        private Point? startPoint;
+        private Point? endPoint;
+        private LineType type;
+        public bool Selected { get; set; } = false;
+        public Line()
+        {
+            startPoint = null;
+            endPoint = null;
+        }
+        public Line(Point? startPoint, Point? endPoint, LineType type)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.type = type;
+        }
+        public bool hitTest(Point point)
+        {
+        // Сделать так, чтобы линия не выделялась, если точка принадлежит прямой, но не принадлежит отрезку-линии.
+            const int HitRange = 5;
+            double distance = Math.Abs((endPoint.Value.Y - startPoint.Value.Y) * point.X - (endPoint.Value.X - startPoint.Value.X) * point.Y + endPoint.Value.X * startPoint.Value.Y - endPoint.Value.Y * startPoint.Value.X) /
+                          Math.Sqrt(Math.Pow(endPoint.Value.Y - startPoint.Value.Y, 2) + Math.Pow(endPoint.Value.X - startPoint.Value.X, 2));
+
+            return distance <= HitRange;
+        }
+        public void Draw(Graphics g)
+        {
+            Pen pen = Selected ? Pens.Red : Pens.Black;
+            g.DrawLine(pen, startPoint.Value, endPoint.Value);
+        }
+    }
+    public enum LineType
+    {
+        Straight,
+        Curve
+    }
+}
+
